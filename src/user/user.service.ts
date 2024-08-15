c9f0926aacd647c3722a84abfc2065b952732e58
Ivan Chebotnov <cheived55@gmail.com>
2024-08-15 21:11:28 +0400

    add user crud api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '@prisma/prisma.service';
+import { genSalt, genSaltSync, hashSync } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly PrismaService: PrismaService) {}
+
+  save(user: Partial<User>) {
+    const hashedPassword = this.hashPassword(user.password);
+    return this.PrismaService.user.create({
+      data: {
+        email: user.email,
+        password: hashedPassword,
+        roles: ['USER'],
+      },
+    });
+  }
+
+  findOne(idOrEmail: string) {
+    return this.PrismaService.user.findFirst({
+      where: {
+        OR: [{ id: idOrEmail }, { email: idOrEmail }],
+      },
+    });
+  }
+
+  delete(id) {
+    return this.PrismaService.user.delete({
+      where: { id },
+    });
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10));
+  }
+}
